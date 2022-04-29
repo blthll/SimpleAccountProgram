d41b4fec40d9080a08bb0e6c640dedf5188421ee
blthll <bolat.halil@std.izu.edu.tr>
2022-04-29 16:03:48 +0300

    initial commit

---
+++ src/main/java/com/sonhal/simpleaccountantprogram/SimpleAccountantProgramApplication.java
@@ -0,0 +1,13 @@
+package com.sonhal.simpleaccountantprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAccountantProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAccountantProgramApplication.class, args);
+    }
+
+}
